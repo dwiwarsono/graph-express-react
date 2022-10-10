86ce57df619c97df57112b0d876b89e0081fd0c4
blackbox <blackbox@blackboxs-MacBook-Pro.local>
2022-10-10 11:31:59 +0700

    Add cors devDependecies at server

--- server/app.js
+++ server/app.js
@@ -2,11 +2,15 @@ const express = require("express");
 const graphqlHTTP = require("express-graphql").graphqlHTTP;
 const schema = require("./schema/schema");
 const mongoose = require("mongoose");
+const cors = require("cors");
+
+
 
 
 
 const app = express();
 
+app.use(cors())
 // CONNECT TO MONGODB DATABASE
 // const url = "mongodb+srv://dwi:theBRIV057@cluster0.ev1dmmn.mongodb.net/graphql?retryWrites=true&w=majority";
 const url = "mongodb://dwi:theBRIV057@ac-cumjwk9-shard-00-00.ev1dmmn.mongodb.net:27017,ac-cumjwk9-shard-00-01.ev1dmmn.mongodb.net:27017,ac-cumjwk9-shard-00-02.ev1dmmn.mongodb.net:27017/graphql?ssl=true&replicaSet=atlas-8zdq9h-shard-0&authSource=admin&retryWrites=true&w=majority";
